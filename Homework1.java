55c435f9067fe0387600bf531a97ee7c18ecc6fa
PavelStark <132998912+PavelStark@users.noreply.github.com>
2023-06-29 18:12:30 +0300

    Add files via upload

---
+++ Homework1.java
@@ -0,0 +1,16 @@
+package de.telran.Homework;
+
+public class Homework1 {
+        public static void main(String[] args) {
+            int myInt2 = 2;
+            our();
+            System.out.println(myInt2);
+        }
+
+        static void our() {
+            int myInt = 1;
+            System.out.println("I method our()");
+            System.out.println(myInt);
+        }
+    }
+
